86c14d7ccd151d55e5e7d2a1056d90de6bf9b597
trancenoid <shivansh.bhr@gmail.com>
2023-01-11 22:03:44 +0530

    add image window and painter

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,115 +1,197 @@
+
+import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'package:image/image.dart' as imagelib;
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const SD2Canvas());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SD2Canvas extends StatelessWidget {
+  const SD2Canvas({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Image Painter Example',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const SD2CanvasApp(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class SD2CanvasApp extends StatefulWidget {
+  const SD2CanvasApp({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  SD2CanvasAppState createState() => SD2CanvasAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+
+class SD2CanvasAppState extends State<SD2CanvasApp> {
+  List<Offset> points = [];
+  List<double> currTransform = [0.0, 0.0, 1.0]; // [x,y,scale]
+  late TapDownDetails doubleTapDetails;
+  late TransformationController tfmController;
+  bool isZoomed = false;
+  late ui.Image mainImage;
+
+  Future<ui.Image> loadImage(String path) async {
+    final ByteData bytes = await rootBundle.load(path);
+    ui.Image decodedImage = await decodeImageFromList(bytes.buffer.asUint8List());
+    return decodedImage;
+  }
+
+  Future<Widget> uiImageToWidget(ui.Image img) async {
+    final bytes = await mainImage.toByteData(format: ui.ImageByteFormat.png);
+
+    return Image.memory(
+        Uint8List.view(bytes!.buffer)
+    );
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+  void initState() {
+    super.initState();
+    mainImage = loadImage("assets/sample.png") as ui.Image;
+    tfmController = TransformationController();
+  }
+
+  Offset transformOffset(Offset o) {
+    double dx = o.dx;
+    double dy = o.dy;
+    return Offset((dx - currTransform[0]) / currTransform[2],
+        (dy - currTransform[1]) / currTransform[2]);
+  }
+
+  Widget _makeCanvas() {
+    return GestureDetector(
+      onDoubleTapDown: (details) {
+        setState(() {
+          doubleTapDetails = details;
+          isZoomed = !isZoomed;
+        });
+      },
+      onDoubleTap: () {
+        Matrix4 zoomed = Matrix4.identity();
+        double x, y, scale;
+        if (!isZoomed) {
+          scale = 2.0;
+          x = -doubleTapDetails.localPosition.dx * (scale - 1);
+          y = -doubleTapDetails.localPosition.dy * (scale - 1);
+          zoomed
+            ..translate(x, y)
+            ..scale(scale);
+        } else {
+          x = y = 0;
+          scale = 1.0;
+        }
+
+        setState(() {
+          currTransform = [x, y, scale];
+        });
+        tfmController.value = zoomed;
+      },
+      child: InteractiveViewer(
+        onInteractionUpdate: (event) {
+          setState(() {
+            if (event.pointerCount == 1) {
+              points.add(transformOffset(event.localFocalPoint));
+              print(points[points.length - 1]);
+            }
+          });
+        },
+        onInteractionStart: (event) {
+          setState(() {
+            if (event.pointerCount == 1) {
+              points.add(transformOffset(event.localFocalPoint));
+              print(points[points.length - 1]);
+            }
+          });
+        },
+        clipBehavior: Clip.none,
+        panEnabled: false,
+        scaleEnabled: false,
+        transformationController: tfmController,
+        child: Stack(
+          children: [
+            Center(
+              child: uiImageToWidget(mainImage),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            CustomPaint(
+              size : Size(mainImage.width as double, mainImage.height as double),
+              painter: CanvasMaskPainter(points),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("SD2 Canvas"),
+      ),
+      body: Column(
+        children: [
+          ClipRect(
+            child: Align(
+              widthFactor: 1.0,
+              heightFactor: 1.0,
+              alignment: Alignment.center,
+              child: _makeCanvas(),
+            ),
+          ),
+          // TextFormField(initialValue: "Realistic Landscape, Artstation",),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: [
+              MaterialButton(
+                onPressed: () async {
+                  ImagePicker picker = ImagePicker();
+                  XFile? image =
+                      await picker.pickImage(source: ImageSource.gallery);
+                  setState(() {
+                    mainImage = Image.file(File(image!.path));
+                  });
+                },
+                color: Colors.blue,
+                child: const Text("Select Image"),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class CanvasMaskPainter extends CustomPainter {
+  List<Offset> points;
+
+  CanvasMaskPainter(this.points);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint1 = Paint()
+      ..color = const Color(0xff63aa65)
+      ..strokeWidth = 10;
+
+    canvas.clipRect(Rect.fromLTWH(0, 0, size.width, size.height));
+    canvas.drawPoints(ui.PointMode.points, points, paint1);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
